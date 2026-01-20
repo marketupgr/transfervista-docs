10d46f48c4af19603627c2cc92ae5e08f76e5573
Nikos <20658439+esprssoo@users.noreply.github.com>
2026-01-20 10:57:40 +0200

    clean

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,51 +17,28 @@ export default defineConfig({
           label: "English",
           lang: "en",
         },
-          el: {
-            label: "Ελληνικά",
-            lang: "el",
-          },
-          es: {
-            label: "Español",
-            lang: "es",
-          },
-          fr: {
-            label: "Français",
-            lang: "fr",
-          },
-          it: {
-            label: "Italiano",
-            lang: "it",
-          },
-          de: {
-            label: "Deutsch",
-            lang: "de",
-          }
+        el: {
+          label: "Ελληνικά",
+          lang: "el",
+        },
+        es: {
+          label: "Español",
+          lang: "es",
+        },
+        fr: {
+          label: "Français",
+          lang: "fr",
+        },
+        it: {
+          label: "Italiano",
+          lang: "it",
+        },
+        de: {
+          label: "Deutsch",
+          lang: "de",
+        },
       },
-      sidebar: [
-        {
-          label: "Get started",
-          translations: {
-            el: "Ξεκινώντας",
-            es: "Empezar",
-            fr: "Commencer",
-            it: "Iniziare",
-            de: "Anfangen",
-          },
-          autogenerate: { directory: "guides" },
-        },
-        {
-          label: "Navigation",
-          translations: {
-            el: "Μενού πλοήγησης",
-            es: "Navegación",
-            fr: "Navigation",
-            it: "Navigazione",
-            de: "Navigationsleiste",
-          },
-          autogenerate: { directory: "navigation" },
-        },
-      ],
+      sidebar: [],
       social: [
         {
           icon: "document",
